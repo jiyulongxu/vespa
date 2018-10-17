34c5d9fe970358d18354d4688f920f965c6cd7a3
Valerij Fredriksen <valerijf@oath.com>
2018-10-17 16:25:54 +0200

    Remove unused code

--- node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/component/ConfigServerInfo.java
+++ node-admin/src/main/java/com/yahoo/vespa/hosted/node/admin/component/ConfigServerInfo.java
@@ -2,8 +2,6 @@
 package com.yahoo.vespa.hosted.node.admin.component;
 
 import com.yahoo.vespa.athenz.api.AthenzService;
-import com.yahoo.vespa.athenz.utils.AthenzIdentities;
-import com.yahoo.vespa.hosted.node.admin.config.ConfigServerConfig;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -19,20 +17,12 @@ import static java.util.stream.Collectors.toMap;
  * @author hakon
  */
 public class ConfigServerInfo {
-    private final List<String> configServerHostNames;
     private final URI loadBalancerEndpoint;
     private final Map<String, URI> configServerURIs;
     private final AthenzService configServerIdentity;
 
-    // TODO: Remove
-    public ConfigServerInfo(ConfigServerConfig config) {
-        this(config.loadBalancerHost(), config.hosts(), config.scheme(), config.port(),
-                (AthenzService) AthenzIdentities.from(config.configserverAthenzIdentity()));
-    }
-
     public ConfigServerInfo(String loadBalancerHostName, List<String> configServerHostNames,
                             String scheme, int port, AthenzService configServerAthenzIdentity) {
-        this.configServerHostNames = configServerHostNames;
         this.configServerURIs = createConfigServerUris(scheme, configServerHostNames, port);
         this.loadBalancerEndpoint = createLoadBalancerEndpoint(loadBalancerHostName, scheme, port);
         this.configServerIdentity = configServerAthenzIdentity;
@@ -40,10 +30,6 @@ public class ConfigServerInfo {
 
     private static URI createLoadBalancerEndpoint(String loadBalancerHost, String scheme, int port) {
         return URI.create(scheme + "://" + loadBalancerHost + ":" + port);
-    }
-
-    public List<String> getConfigServerHostNames() {
-        return configServerHostNames;
     }
 
     public List<URI> getConfigServerUris() {
